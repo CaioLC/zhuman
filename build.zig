5f1c676bdb652b267fe60b9b0da1eb26f63133f0
Caio Castro <lcastro.caio@gmail.com>
2026-01-18 22:08:28 -0300

    ttf rendering finally

---
+++ build.zig
@@ -0,0 +1,76 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    // const linkage = b.option(
+    //     std.builtin.LinkMode,
+    //     "linkage",
+    //     "`.static` or `.dynamic` (default is `.dynamic`)",
+    // ) orelse .dynamic;
+
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // My lib
+    const ha_mod = b.addModule("human_action", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe_mod.addImport("ha", ha_mod);
+
+    // SDL dependency
+    const sdl3 = b.dependency("sdl3", .{
+        .target = target,
+        .optimize = optimize,
+
+        // Lib options.
+        // .callbacks = false,
+        // .ext_image = false,
+        // .ext_net = false,
+        .ext_ttf = true,
+        // .log_message_stack_size = 1024,
+        // .main = false,
+        // .renderer_debug_text_stack_size = 1024,
+
+        // Options passed directly to https://github.com/castholm/SDL (SDL3 C Bindings):
+        // .c_sdl_preferred_linkage = .static,
+        // .c_sdl_strip = false,
+        // .c_sdl_sanitize_c = .off,
+        // .c_sdl_lto = .none,
+        // .c_sdl_emscripten_pthreads = false,
+        // .c_sdl_install_build_config_h = false,
+
+        // Options if `ext_image` is enabled:
+        // .image_enable_bmp = true,
+        // .image_enable_gif = true,
+        // .image_enable_jpg = true,
+        // .image_enable_lbm = true,
+        // .image_enable_pcx = true,
+        // .image_enable_png = true,
+        // .image_enable_pnm = true,
+        // .image_enable_qoi = true,
+        // .image_enable_svg = true,
+        // .image_enable_tga = true,
+        // .image_enable_xcf = true,
+        // .image_enable_xpm = true,
+        // .image_enable_xv = true,
+    });
+    exe_mod.addImport("sdl3", sdl3.module("sdl3"));
+
+    // Build for desktop.
+    const exe = b.addExecutable(.{
+        .name = "human_action",
+        .root_module = exe_mod,
+    });
+    b.installArtifact(exe);
+
+    // Run App
+    const run_exe = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_exe.step);
+}
