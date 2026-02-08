62ef8657978371e0d515e847ac79264479e59e5e
Caio Lacerda de Castro <lcastro.caio@gmail.com>
2026-02-08 18:03:49 -0300

    fixed executable dependencies

--- src/main.zig
+++ src/main.zig
@@ -1,26 +1,26 @@
 const std = @import("std");
 
-const sdl3 = @import("sdl3");
-const Renderer = sdl3.render.Renderer;
+const ha = @import("ha");
+const time = ha.time;
+const ui = ha.ui;
+const sdl = ha.sdl;
+const zfont = ha.font;
 
-const time = @import("./time.zig");
-const ui = @import("./ui.zig");
-
+const Renderer = ha.sdl.render.Renderer;
 const screen_width: c_int = 800;
 const screen_height: c_int = 600;
 const fps = 60;
 const font_path = "assets/fonts/Kenney Mini Square.ttf";
 
 // logger
-const log_app = sdl3.log.Category.application;
+const log_app = sdl.log.Category.application;
 
 // colors
-const zfont = @import("./font.zig");
 const white = zfont.white;
 
 const Resources = struct {
     quit_app: bool,
-    font: sdl3.ttf.Font,
+    font: sdl.ttf.Font,
     screen_height: c_int,
     screen_width: c_int,
     ui_root: ?*ui.Node,
@@ -28,7 +28,7 @@ const Resources = struct {
     pub fn init() !Resources {
         return .{
             .quit_app = false,
-            .font = try sdl3.ttf.Font.init(font_path, 24),
+            .font = try sdl.ttf.Font.init(font_path, 24),
             .screen_height = screen_height,
             .screen_width = screen_width,
             .ui_root = null,
@@ -54,16 +54,16 @@ pub fn main() !void {
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    try sdl3.init(.{ .video = true, .events = true });
-    defer sdl3.quit(.{ .video = true, .events = true });
-    try sdl3.ttf.init();
-    defer sdl3.ttf.quit();
+    try sdl.init(.{ .video = true, .events = true });
+    defer sdl.quit(.{ .video = true, .events = true });
+    try sdl.ttf.init();
+    defer sdl.ttf.quit();
 
-    const window, const renderer = try sdl3.render.Renderer.initWithWindow("Human Action", screen_width, screen_height, .{ .resizable = true });
+    const window, const renderer = try sdl.render.Renderer.initWithWindow("Human Action", screen_width, screen_height, .{ .resizable = true });
     defer renderer.deinit();
     defer window.deinit();
 
-    var frame_capper = sdl3.extras.FramerateCapper(f32){ .mode = .{ .unlimited = {} } };
+    var frame_capper = sdl.extras.FramerateCapper(f32){ .mode = .{ .unlimited = {} } };
     renderer.setVSync(.{ .on_each_num_refresh = 1 }) catch {
         frame_capper.mode = .{ .limited = fps };
     };
@@ -93,7 +93,7 @@ pub fn main() !void {
 }
 
 fn events(res: *Resources, obj: *Objects) !void {
-    while (sdl3.events.poll()) |event| {
+    while (sdl.events.poll()) |event| {
         switch (event) {
             .quit, .terminating => res.quit_app = true,
             .window_resized => |e| {
@@ -206,7 +206,7 @@ fn render(renderer: Renderer, res: Resources, _: Objects) !void {
         try root.collect(allocator, &node_stack);
 
         for (node_stack.items) |node| {
-            const dst = sdl3.rect.FRect{
+            const dst = sdl.rect.FRect{
                 .x = node.global_x.?,
                 .y = node.global_y.?,
                 .h = node.height,
--- src/root.zig
+++ src/root.zig
@@ -2,8 +2,11 @@
 comptime {
     // This will ensure that the file 'api.zig' is always discovered (as long as this file is discovered).
     // It is useful if 'api.zig' contains important exported declarations.
-    _ = @import("sdl3");
-    _ = @import("./ui.zig");
+    // _ = @import("sdl3");
 }
-const std = @import("std");
+pub const sdl = @import("sdl3");
+pub const ui = @import("./ui.zig");
+pub const time = @import("./time.zig");
+pub const font = @import("./font.zig");
+
 
